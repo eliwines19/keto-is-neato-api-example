0c2629ca1a3593d15f252555c64cf02284cc9d4f
Elijah Wines <wines.elijah@gmail.com>
2023-06-06 19:11:01 -0400

    successfully gathered recipe data from API

--- script/index.js
+++ script/index.js
@@ -1,5 +1,11 @@
 window.onload = () => {
-    
-    console.log('it loaded')
+
+    // getting the recipes
+    let recipes;
+    fetch('https://keto-is-neato-backend.herokuapp.com/api/v1/recipes')
+    .then(response => response.json())
+    .then(data => {
+        recipes = data.data
+    })
 
 }
